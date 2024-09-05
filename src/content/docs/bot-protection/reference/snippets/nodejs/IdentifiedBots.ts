11c29a02972e8abd2f22589262d051e033a425c4
Blaine Bublitz <blaine@arcjet.com>
2024-09-05 12:56:38 -0400

    improve identified bots examples

--- src/content/docs/bot-protection/reference/snippets/nodejs/IdentifiedBots.ts
+++ src/content/docs/bot-protection/reference/snippets/nodejs/IdentifiedBots.ts
@@ -16,18 +16,15 @@ const server = http.createServer(async function (
   res: http.ServerResponse,
 ) {
   const decision = await aj.protect(req);
-  console.log("Arcjet decision", decision);
 
-  if (decision.isDenied() && decision.reason.isBot()) {
+  if (decision.reason.isBot()) {
     console.log("detected + allowed bots", decision.reason.allowed);
     console.log("detected + denied bots", decision.reason.denied);
+  }
 
+  if (decision.isDenied()) {
     res.writeHead(403, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        error: "Forbidden",
-      }),
-    );
+    res.end(JSON.stringify({ error: "Forbidden" }));
   } else {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Hello world" }));
