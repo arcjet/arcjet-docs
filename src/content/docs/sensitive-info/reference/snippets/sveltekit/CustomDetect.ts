c9c677c64a569c82f8c1c794df1d1a71ea5392d2
e-moran <eoin@arcjet.com>
2024-09-08 22:11:32 +0100

    docs: copy custom detect explainer to all adapters

---
+++ src/content/docs/sensitive-info/reference/snippets/sveltekit/CustomDetect.ts
@@ -0,0 +1,42 @@
+import { env } from "$env/dynamic/private";
+import arcjet, { sensitiveInfo } from "@arcjet/sveltekit";
+import { error, json, type RequestEvent } from "@sveltejs/kit";
+
+// This function is called by the `sensitiveInfo` rule to perform custom detection on strings.
+function detectDash(tokens: string[]): Array<"CONTAINS_DASH" | undefined> {
+  return tokens.map((token) => {
+    if (token.includes("-")) {
+      return "CONTAINS_DASH";
+    }
+  });
+}
+
+const aj = arcjet({
+  key: env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
+  rules: [
+    sensitiveInfo({
+      deny: ["EMAIL", "CONTAINS_DASH"],
+      mode: "LIVE",
+      detect: detectDash,
+      contextWindowSize: 2,
+    }),
+  ],
+});
+
+export async function GET(event: RequestEvent) {
+  const decision = await aj.protect(event);
+
+  for (const result of decision.results) {
+    console.log("Rule Result", result);
+
+    if (result.reason.isSensitiveInfo()) {
+      console.log("Sensitive info rule", result);
+    }
+  }
+
+  if (decision.isDenied()) {
+    return error(403, "Forbidden");
+  }
+
+  return json({ message: "Hello world" });
+}
