cbd44fb66ab4f6613fcc9fac4e022930ebbb14e8
David Mytton <david@arcjet.com>
2024-09-16 12:05:17 -0400

    chore: Improve the docs for how to consume the body in Express when using sensitiveInfo rules (#74)
    * chore: Improve the docs for how to consume the body in Express when using sensitiveInfo rules
    
    * Update src/content/docs/sensitive-info/reference/nodejs.mdx
    
    Co-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
    
    * Update src/content/docs/troubleshooting.mdx
    
    Co-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>

---
+++ src/snippets/sensitive-info/reference/nodejs/AccessBody.ts
@@ -0,0 +1,41 @@
+import arcjet, { sensitiveInfo } from "@arcjet/node";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    sensitiveInfo({
+      mode: "LIVE",
+      deny: ["EMAIL"],
+    }),
+  ],
+});
+
+// Body is accessed here first so it can be used in the protect method and
+// referenced later.
+app.use(express.text());
+
+app.post("/", async (req, res) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied() && decision.reason.isSensitiveInfo()) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        error: "Sensitive Information Detected",
+        denied: decision.reason.denied,
+      }),
+    );
+  } else {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    // We can safely access the body here because it has already been referenced
+    res.end(JSON.stringify({ message: `You said: ${req.body}` }));
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
