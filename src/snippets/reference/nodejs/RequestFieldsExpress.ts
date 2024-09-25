b0c06cf8dcce4cf44825cdec2fc020414c36448f
David Mytton <david@arcjet.com>
2024-09-25 10:31:58 +0100

    Address comments

--- src/snippets/reference/nodejs/RequestFieldsExpress.ts
+++ src/snippets/reference/nodejs/RequestFieldsExpress.ts
@@ -1,6 +1,7 @@
-import arcjet, { fixedWindow, shield } from "@arcjet/node";
-import express from "express";
 import { readBody } from "@arcjet/body";
+import ArcjetHeaders from "@arcjet/headers";
+import arcjet, { fixedWindow, shield } from "arcjet";
+import express from "express";
 
 const app = express();
 const port = 3000;
@@ -9,8 +10,8 @@ const aj = arcjet({
   // Get your site key from https://app.arcjet.com and set it as an environment
   // variable rather than hard coding.
   key: process.env.ARCJET_KEY!,
-  // Limiting by ip.src is the default if not specified
-  //characteristics: ["ip.src"],
+  // Limiting by ip.src is the default if not specified characteristics:
+  //["ip.src"],
   rules: [
     // Protect against common attacks with Arcjet Shield
     shield({
@@ -30,15 +31,31 @@ app.get("/", async (req, res) => {
   // If you are using standard Express, you can just pass req directly to
   // protect(). If you want to do something custom, you can construct an object
   // with the necessary details, such as the following:
+  const headers = new ArcjetHeaders(req.headers);
   const details = {
     ip: req.ip,
     method: req.method,
-    host: req.hostname,
-    url: req.path,
-    headers: req.headers,
+    host: req.headers.host,
+    path: req.path,
+    headers,
   };
 
-  const decision = await aj.protect(details);
+  // Provide a function to get the body in case it is needed by a rule.
+  const getBody = async () => {
+    try {
+      return await readBody(req, {
+        // Only read up to 1 MiB of the body
+        limit: 1048576,
+      });
+    } catch {
+      // Return undefined in case of failure to allow the rule that called
+      // `getBody` handle it.
+      return undefined;
+    }
+  };
+
+  const decision = await aj.protect({ getBody }, details);
+  console.log(decision);
 
   if (decision.isDenied()) {
     res.writeHead(429, { "Content-Type": "application/json" });
