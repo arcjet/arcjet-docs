d01e46b4874f01bdb46f7cfd5037ce5dcb2a403c
David Mytton <david@arcjet.com>
2024-10-02 08:40:26 +0100

    chore: Remove match option from rate limit config

--- src/snippets/rate-limiting/reference/nodejs/TokenBucket.ts
+++ src/snippets/rate-limiting/reference/nodejs/TokenBucket.ts
@@ -6,7 +6,6 @@ const aj = arcjet({
   rules: [
     tokenBucket({
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      match: "/api/hello", // match all requests to /api/hello
       refillRate: 10, // refill 10 tokens per interval
       interval: 60, // 60 second interval
       capacity: 100, // bucket maximum capacity of 100 tokens
--- src/snippets/rate-limiting/reference/sveltekit/HookGlobal.ts
+++ src/snippets/rate-limiting/reference/sveltekit/HookGlobal.ts
@@ -7,8 +7,6 @@ const aj = arcjet({
   rules: [
     fixedWindow({
       mode: "LIVE",
-      // no match means it runs on every route
-      //match: "/api",
       window: "1h",
       max: 60,
     }),
