b952e7fb01be345f9eff683630260bae99d0a1f5
Morekid <daniele.dematteis@gmail.com>
2024-10-07 16:57:55 +0200

    add sorting utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -52,3 +52,41 @@ export const kebabToCamel = (str: string) => {
     )
     .join("");
 };
+
+/**
+ * Sorting
+ * --------------------------------------------------------------------------------------------------------------
+ */
+export const descendingComparator = <T>(a: T, b: T, orderBy: keyof T) => {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+};
+
+export type Order = "asc" | "desc";
+
+export function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+export function getStringComparator(
+  order: Order,
+): (a: string, b: string) => number {
+  return order === "desc"
+    ? (a, b) => (a > b ? -1 : a < b ? 1 : 0)
+    : (a, b) => (a > b ? 1 : a < b ? -1 : 0);
+}
+
+/** END: Sorting utils */
