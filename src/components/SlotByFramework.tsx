e26e7eb60e332b1f8b6e655698e7f6c7d918c64f
Morekid <daniele.dematteis@gmail.com>
2024-10-18 15:43:59 +0200

    fix erratic react comp rendering: don't forwardRef

--- src/components/SlotByFramework.tsx
+++ src/components/SlotByFramework.tsx
@@ -3,8 +3,8 @@ import type { FrameworkKey } from "@/lib/prefs";
 import { extractSlotContent } from "@/lib/utils";
 import { displayedFramework } from "@/store";
 import { useStore } from "@nanostores/react";
-import type { ForwardedRef, PropsWithChildren } from "react";
-import { forwardRef, useEffect, useMemo, useState } from "react";
+import type { PropsWithChildren } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import "@/components/SlotByFramework.scss";
 
@@ -19,44 +19,40 @@ interface Props extends PropsWithChildren {
  *
  * @param inline - Renders the content without a wrapping element.
  */
-const SlotByFramework = forwardRef(
-  ({ inline, ...props }: Props, ref: ForwardedRef<HTMLDivElement>) => {
-    const $displayedFramework = useStore(displayedFramework);
+const SlotByFramework = ({ inline, ...props }: Props) => {
+  const $displayedFramework = useStore(displayedFramework);
 
-    // The selected framework
-    const [selectedFramework, setSelectedFramework] = useState<FrameworkKey>();
+  // The selected framework
+  const [selectedFramework, setSelectedFramework] = useState<FrameworkKey>();
 
-    useEffect(() => {
-      setSelectedFramework($displayedFramework);
-    }, [$displayedFramework]);
+  useEffect(() => {
+    setSelectedFramework($displayedFramework);
+  }, [$displayedFramework]);
 
-    const content = useMemo(() => {
-      return (
-        <>
-          {selectedFramework && extractSlotContent(props, selectedFramework)}
-          {props.children}
-        </>
-      );
-    }, [selectedFramework]);
-
-    // Loading handling
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-      if (selectedFramework) setLoading(false);
-    }, [selectedFramework]);
-
-    return loading ? (
-      <Skeleton radius={1} appearance="Fade" />
-    ) : inline ? (
-      content
-    ) : (
-      <div ref={ref} className="SlotByFramework">
-        {content}
-      </div>
+  const content = useMemo(() => {
+    return (
+      <>
+        {selectedFramework && extractSlotContent(props, selectedFramework)}
+        {props.children}
+      </>
     );
-  },
-);
+  }, [selectedFramework]);
+
+  // Loading handling
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (selectedFramework) setLoading(false);
+  }, [selectedFramework]);
+
+  return loading ? (
+    <Skeleton radius={1} appearance="Fade" />
+  ) : inline ? (
+    content
+  ) : (
+    <div className="SlotByFramework">{content}</div>
+  );
+};
 SlotByFramework.displayName = "SlotByFramework";
 
 export default SlotByFramework;
