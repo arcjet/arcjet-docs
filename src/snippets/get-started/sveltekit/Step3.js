f8d95bca5daf6128cea7a7540c296e3b97b4b7c2
Morekid <daniele.dematteis@gmail.com>
2024-11-04 16:53:25 -0500

    remove leftover function

--- src/snippets/get-started/sveltekit/Step3.js
+++ src/snippets/get-started/sveltekit/Step3.js
@@ -25,23 +25,6 @@ const aj = arcjet({
   ],
 });
 
-export async function handle({ event, resolve }) {
-  const decision = await aj.protect(event, { requested: 5 }); // Deduct 5 tokens from the bucket
-  console.log("Arcjet decision", decision);
-
-  if (decision.isDenied()) {
-    if (decision.reason.isRateLimit()) {
-      return error(429, "Too Many Requests");
-    } else if (decision.reason.isBot()) {
-      return error(403, "No Bots Allowed");
-    } else {
-      return error(403, "Forbidden");
-    }
-  }
-
-  return resolve(event);
-}
-
 export async function GET(event) {
   const decision = await aj.protect(event, { requested: 5 }); // Deduct 5 tokens from the bucket
   console.log("Arcjet decision", decision);
