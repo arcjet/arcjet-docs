1e5fce77946b6375123642fa02ded58671a3f629
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2024-11-07 15:50:16 -0700

    fix: Convert all snippet imports to relative paths & resolve incorrect imports (#228)

---
+++ src/snippets/bot-protection/reference/nextjs/Middleware.js
@@ -0,0 +1,19 @@
+import arcjet, { createMiddleware, detectBot } from "@arcjet/next";
+export const config = {
+  // matcher tells Next.js which routes to run the middleware on.
+  // This runs the middleware on all routes except for static assets.
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
+const aj = arcjet({
+  key: process.env.ARCJET_KEY, // Get your site key from https://app.arcjet.com
+  rules: [
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      // Block all bots except search engine crawlers. See the full list of bots
+      // for other options: https://arcjet.com/bot-list
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+  ],
+});
+// Pass any existing middleware with the optional existingMiddleware prop
+export default createMiddleware(aj);
