5bf34b429ec13f3d5a7e6c458c439bc6777f68e7
blaine-arcjet <146491715+blaine-arcjet@users.noreply.github.com>
2024-11-07 15:44:02 -0700

    feat: Add Deno Bot Protection Quick Start (#215)

---
+++ src/snippets/bot-protection/quick-start/deno/Step3.ts
@@ -0,0 +1,28 @@
+import "jsr:@std/dotenv/load";
+
+import arcjet, { detectBot } from "@arcjet/deno";
+
+const aj = arcjet({
+  key: Deno.env.get("ARCJET_KEY")!, // Get your site key from https://app.arcjet.com
+  rules: [
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      // Block all bots except search engine crawlers. See the full list of bots
+      // for other options: https://arcjet.com/bot-list
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+  ],
+});
+
+Deno.serve(
+  { port: 3000 },
+  aj.handler(async (req) => {
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied()) {
+      return new Response("Forbidden", { status: 403 });
+    }
+
+    return new Response("Hello world");
+  }),
+);
