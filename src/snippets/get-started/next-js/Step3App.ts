ce2a9a9ace8e0b63cf20da94f58abcafe6b709fb
David Mytton <david@arcjet.com>
2024-11-13 15:42:21 +0000

    chore: Change default bot rules to show some other common options (#256)

--- src/snippets/get-started/next-js/Step3App.ts
+++ src/snippets/get-started/next-js/Step3App.ts
@@ -10,9 +10,14 @@ const aj = arcjet({
     // Create a bot detection rule
     detectBot({
       mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-      // Block all bots except search engine crawlers. See
-      // https://arcjet.com/bot-list
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
     }),
     // Create a token bucket rate limit. Other algorithms are supported.
     tokenBucket({
