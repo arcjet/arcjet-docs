a401d1c24962345b5a2f2801322c790f72c14093
David Mytton <david@arcjet.com>
2024-11-13 15:38:28 +0000

    chore: Change default bot rules to show some other common options

--- src/snippets/bot-protection/quick-start/bun/Step3.js
+++ src/snippets/bot-protection/quick-start/bun/Step3.js
@@ -6,9 +6,14 @@ const aj = arcjet({
   rules: [
     detectBot({
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      // Block all bots except search engine crawlers. See the full list of bots
-      // for other options: https://arcjet.com/bot-list
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
     }),
   ],
 });
