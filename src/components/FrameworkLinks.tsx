65652c861e12ed1ebd86866cb935b6b3331196f0
Morekid <daniele.dematteis@gmail.com>
2025-02-03 13:27:41 +0100

    FrameworkLinks: extend to allow exclusions

--- src/components/FrameworkLinks.tsx
+++ src/components/FrameworkLinks.tsx
@@ -6,7 +6,7 @@ import { NextJs as IconNextJs } from "@/components/icons/tech/NextJs";
 import { NodeJs as IconNodeJs } from "@/components/icons/tech/NodeJs";
 import { Remix as IconRemix } from "@/components/icons/tech/Remix";
 import { SvelteKit as IconSvelteKit } from "@/components/icons/tech/SvelteKit";
-import { getStoredFramework } from "@/lib/prefs";
+import { getStoredFramework, type FrameworkKey } from "@/lib/prefs";
 import { queryParamFramework } from "@/store";
 import { useStore } from "@nanostores/react";
 import type { ForwardedRef, PropsWithChildren } from "react";
@@ -22,11 +22,12 @@ import styles from "./FrameworkLinks.module.scss";
 
 interface FrameworkLinksProps extends PropsWithChildren {
   title?: string;
+  exclude?: FrameworkKey[];
 }
 
 const FrameworkLinks = forwardRef(
   (
-    { title = "Choose a framework", ...props }: FrameworkLinksProps,
+    { title = "Choose a framework", exclude, ...props }: FrameworkLinksProps,
     ref: ForwardedRef<HTMLDivElement>,
   ) => {
     const [hide, setHide] = useState(true);
@@ -54,86 +55,106 @@ const FrameworkLinks = forwardRef(
         <div ref={ref} className={cls} {...props}>
           <h2 id="choose-a-framework">{title}</h2>
           <div className={styles.Links}>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=bun"
-              decoratorLeft={<IconBun />}
-            >
-              Bun
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=bun-hono"
-              decoratorLeft={<IconBun />}
-            >
-              Bun + Hono
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=deno"
-              decoratorLeft={<IconDeno />}
-            >
-              Deno
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=nest-js"
-              decoratorLeft={<IconNestJs />}
-            >
-              NestJS
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=next-js"
-              decoratorLeft={<IconNextJs />}
-            >
-              Next.js
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=node-js"
-              decoratorLeft={<IconNodeJs />}
-            >
-              Node.js
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=node-js-express"
-              decoratorLeft={<IconNodeJs />}
-            >
-              Node.js + Express
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=node-js-hono"
-              decoratorLeft={<IconNodeJs />}
-            >
-              Node.js + Hono
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=remix"
-              decoratorLeft={<IconRemix />}
-            >
-              Remix
-            </Button>
-            <Button
-              as="link"
-              size="lg"
-              href="?f=sveltekit"
-              decoratorLeft={<IconSvelteKit />}
-            >
-              SvelteKit
-            </Button>
+            {(!exclude || !exclude.includes("bun")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=bun"
+                decoratorLeft={<IconBun />}
+              >
+                Bun
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("bun-hono")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=bun-hono"
+                decoratorLeft={<IconBun />}
+              >
+                Bun + Hono
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("deno")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=deno"
+                decoratorLeft={<IconDeno />}
+              >
+                Deno
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("nest-js")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=nest-js"
+                decoratorLeft={<IconNestJs />}
+              >
+                NestJS
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("next-js")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=next-js"
+                decoratorLeft={<IconNextJs />}
+              >
+                Next.js
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("node-js")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=node-js"
+                decoratorLeft={<IconNodeJs />}
+              >
+                Node.js
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("node-js-express")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=node-js-express"
+                decoratorLeft={<IconNodeJs />}
+              >
+                Node.js + Express
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("node-js-hono")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=node-js-hono"
+                decoratorLeft={<IconNodeJs />}
+              >
+                Node.js + Hono
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("remix")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=remix"
+                decoratorLeft={<IconRemix />}
+              >
+                Remix
+              </Button>
+            )}
+            {(!exclude || !exclude.includes("sveltekit")) && (
+              <Button
+                as="link"
+                size="lg"
+                href="?f=sveltekit"
+                decoratorLeft={<IconSvelteKit />}
+              >
+                SvelteKit
+              </Button>
+            )}
           </div>
         </div>
       )
