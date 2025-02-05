eee17daa72d27a9a607a0a58b33ac5175f17c417
Daniele De Matteis <632296+morekid@users.noreply.github.com>
2025-02-05 15:44:27 +0100

    feat: page titles by framework (#400)
    Co-authored-by: David Mytton <david@arcjet.com>

--- src/components/TextByFramework.tsx
+++ src/components/TextByFramework.tsx
@@ -1,10 +1,24 @@
 import type { FrameworkKey } from "@/lib/prefs";
+import { getClosestFrameworkMatch, getStoredFramework } from "@/lib/prefs";
 import { extractSlotContent } from "@/lib/utils";
-import { displayedFramework } from "@/store";
+import { availableFrameworks, displayedFramework } from "@/store";
 import { useStore } from "@nanostores/react";
 import type { ForwardedRef, PropsWithChildren } from "react";
 import { forwardRef, useEffect, useMemo, useState } from "react";
 import Skeleton from "./Skeleton";
+
+// The framework mapped texts
+type PropsWithFrameworks = {
+  [key in FrameworkKey]: string;
+};
+
+// The default text
+type PropsWithDefault = {
+  default?: string;
+  onChangeFramework?: (framework: FrameworkKey) => void;
+};
+
+export type Props = PropsWithDefault & PropsWithFrameworks & PropsWithChildren;
 
 /**
  * Text By Framework
@@ -12,31 +26,65 @@ import Skeleton from "./Skeleton";
  * Renders the appropriate text based on the selected framework.
  */
 const TextByFramework = forwardRef(
-  ({ ...props }: PropsWithChildren, ref: ForwardedRef<HTMLElement>) => {
+  (
+    { onChangeFramework, default: defaultText, ...props }: Props,
+    ref: ForwardedRef<HTMLElement>,
+  ) => {
     const $displayedFramework = useStore(displayedFramework);
+    const $availableFrameworks = useStore(availableFrameworks);
 
     // The selected framework
     const [selectedFramework, setSelectedFramework] = useState<FrameworkKey>();
 
+    // Sync with local storage value or displayed framework
     useEffect(() => {
-      setSelectedFramework($displayedFramework);
-    }, [$displayedFramework]);
+      let framework: FrameworkKey | undefined = undefined;
+
+      if (defaultText) {
+        // When a default is provided, set the framework
+        // only if a preference is stored.
+        const storedFramework = getStoredFramework();
+        if (storedFramework) framework = storedFramework;
+      } else {
+        // Else set it from the currently displayed framework
+        framework = $displayedFramework;
+      }
+
+      if (framework) {
+        // Not all stored frameworks may be in the list currently,
+        // so we try to return the closest match
+        const match = getClosestFrameworkMatch(
+          framework,
+          $availableFrameworks.map((f) => f.key),
+        );
+
+        setSelectedFramework(match);
+        if (onChangeFramework) onChangeFramework(match);
+      }
+    }, [
+      $displayedFramework,
+      $availableFrameworks,
+      onChangeFramework,
+      defaultText,
+    ]);
 
     const content = useMemo(() => {
       return (
         <>
-          {selectedFramework && extractSlotContent(props, selectedFramework)}
+          {selectedFramework
+            ? extractSlotContent(props, selectedFramework)
+            : defaultText}
           {props.children}
         </>
       );
-    }, [selectedFramework]);
+    }, [selectedFramework, defaultText]);
 
     // Loading handling
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      if (selectedFramework) setLoading(false);
-    }, [selectedFramework]);
+      if (selectedFramework || defaultText !== undefined) setLoading(false);
+    }, [selectedFramework, defaultText]);
 
     return loading ? <Skeleton as="span" radius={0.5} inline /> : content;
   },
