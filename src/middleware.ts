26dfff0c4962a376b17eb9355c585ef9120c0c59
David Mytton <david@arcjet.com>
2025-06-23 10:10:27 +0000

    Revert "Use Vercel edge middleware"
    This reverts commit fa3c52732d4ef2678da45d2330e5980c2fe2de15.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,16 @@
 import { defineMiddleware } from "astro:middleware";
 import aj from "arcjet:client";
 
-export const onRequest = defineMiddleware(async ({ request }, next) => {
-  const decision = await aj.protect(request);
+export const onRequest = defineMiddleware(
+  async ({ isPrerendered, request }, next) => {
+    if (!isPrerendered) {
+      const decision = await aj.protect(request);
 
-  if (decision.isDenied()) {
-    return new Response(null, { status: 403, statusText: "Forbidden" });
-  }
+      if (decision.isDenied()) {
+        return new Response(null, { status: 403, statusText: "Forbidden" });
+      }
+    }
 
-  return next();
-});
+    return next();
+  },
+);
