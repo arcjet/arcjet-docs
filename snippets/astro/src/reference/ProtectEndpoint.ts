797b63bf6cd8dd1ffd39eb35b8b0bf1f5e609f14
Quinn Blenkinsop <quinn@arcjet.com>
2025-07-08 18:45:24 +0000

    chore: Clarify how arject handles astro's static routes

--- snippets/astro/src/reference/ProtectEndpoint.ts
+++ snippets/astro/src/reference/ProtectEndpoint.ts
@@ -1,6 +1,10 @@
 import aj from "arcjet:client";
 import type { APIRoute } from "astro";
 
+// Not needed in 'server' mode, see:
+// https://docs.astro.build/en/guides/routing/#on-demand-dynamic-routes
+export const prerender = false;
+
 export const POST: APIRoute = async ({ request }) => {
   const decision = await aj.protect(request);
 
