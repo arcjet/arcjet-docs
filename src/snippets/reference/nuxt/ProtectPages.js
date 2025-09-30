b915d6bb9d75730a8afb979f68e684b1c372b4ff
rei <forrest@arcjet.com>
2025-09-30 14:39:41 -0400

    more nonsense

---
+++ src/snippets/reference/nuxt/ProtectPages.js
@@ -0,0 +1,23 @@
+import arcjet, { shield } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    // Protect against common attacks with Arcjet Shield
+    shield({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+    }),
+  ],
+});
+
+export default async function handler(req, res) {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return res
+      .status(403)
+      .json({ error: "Forbidden", reason: decision.reason });
+  }
+
+  res.status(200).json({ name: "Hello world" });
+}
