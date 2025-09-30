a598f3adc5486c7044a269b2ab61f999c5471cb9
Erik Wilson <erik@arcjet.com>
2025-09-30 09:05:37 -0700

    wip

--- src/snippets/get-started/nuxt/Step3Config.js
+++ src/snippets/get-started/nuxt/Step3Config.js
@@ -1,44 +1,42 @@
 // @ts-check
-// @ts-expect-error
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/astro";
+import { defineNuxtConfig } from "nuxt/config";
+// import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/nuxt";
 
-export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
-  env: {
-    validateSecrets: true,
-  },
-  integrations: [
-    arcjet({
-      rules: [
-        // Shield protects your app from common attacks e.g. SQL injection
-        shield({ mode: "LIVE" }),
-        // Create a bot detection rule
-        detectBot({
-          mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-          // Block all bots except the following
-          allow: [
-            "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-            // Uncomment to allow these other common bot categories
-            // See the full list at https://arcjet.com/bot-list
-            //"CATEGORY:MONITOR", // Uptime monitoring services
-            //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-          ],
-        }),
-        // Create a token bucket rate limit. Other algorithms are supported.
-        tokenBucket({
-          mode: "LIVE",
-          // Tracked by IP address by default, but this can be customized
-          // See https://docs.arcjet.com/fingerprints
-          //characteristics: ["ip.src"],
-          refillRate: 5, // Refill 5 tokens per interval
-          interval: 10, // Refill every 10 seconds
-          capacity: 10, // Bucket capacity of 10 tokens
-        }),
-      ],
-    }),
-  ],
+export default defineNuxtConfig({
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  // env: {
+  //   validateSecrets: true,
+  // },
+  // integrations: [
+  //   arcjet({
+  //     rules: [
+  //       // Shield protects your app from common attacks e.g. SQL injection
+  //       shield({ mode: "LIVE" }),
+  //       // Create a bot detection rule
+  //       detectBot({
+  //         mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+  //         // Block all bots except the following
+  //         allow: [
+  //           "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+  //           // Uncomment to allow these other common bot categories
+  //           // See the full list at https://arcjet.com/bot-list
+  //           //"CATEGORY:MONITOR", // Uptime monitoring services
+  //           //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+  //         ],
+  //       }),
+  //       // Create a token bucket rate limit. Other algorithms are supported.
+  //       tokenBucket({
+  //         mode: "LIVE",
+  //         // Tracked by IP address by default, but this can be customized
+  //         // See https://docs.arcjet.com/fingerprints
+  //         //characteristics: ["ip.src"],
+  //         refillRate: 5, // Refill 5 tokens per interval
+  //         interval: 10, // Refill every 10 seconds
+  //         capacity: 10, // Bucket capacity of 10 tokens
+  //       }),
+  //     ],
+  //   }),
+  // ],
 });
