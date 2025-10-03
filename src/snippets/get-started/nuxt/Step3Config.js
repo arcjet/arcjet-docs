a662a17e11fd22f202add1cf9dd465e2f5a11de1
Erik Wilson <Erik.E.Wilson@gmail.com>
2025-10-03 06:47:33 -0700

    Update src/snippets/get-started/nuxt/Step3Config.js
    Co-authored-by: Quinn Blenkinsop <quinn@arcjet.com>

--- src/snippets/get-started/nuxt/Step3Config.js
+++ src/snippets/get-started/nuxt/Step3Config.js
@@ -3,40 +3,10 @@ import { defineNuxtConfig } from "nuxt/config";
 // import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/nuxt";
 
 export default defineNuxtConfig({
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
-  // env: {
-  //   validateSecrets: true,
-  // },
-  // integrations: [
-  //   arcjet({
-  //     rules: [
-  //       // Shield protects your app from common attacks e.g. SQL injection
-  //       shield({ mode: "LIVE" }),
-  //       // Create a bot detection rule
-  //       detectBot({
-  //         mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-  //         // Block all bots except the following
-  //         allow: [
-  //           "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-  //           // Uncomment to allow these other common bot categories
-  //           // See the full list at https://arcjet.com/bot-list
-  //           //"CATEGORY:MONITOR", // Uptime monitoring services
-  //           //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-  //         ],
-  //       }),
-  //       // Create a token bucket rate limit. Other algorithms are supported.
-  //       tokenBucket({
-  //         mode: "LIVE",
-  //         // Tracked by IP address by default, but this can be customized
-  //         // See https://docs.arcjet.com/fingerprints
-  //         //characteristics: ["ip.src"],
-  //         refillRate: 5, // Refill 5 tokens per interval
-  //         interval: 10, // Refill every 10 seconds
-  //         capacity: 10, // Bucket capacity of 10 tokens
-  //       }),
-  //     ],
-  //   }),
-  // ],
+  arcjet: {
+    key: process.env.ARCJET_KEY,
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@arcjet/nuxt"],
 });
