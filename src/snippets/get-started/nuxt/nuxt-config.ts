4e82f58af47a3a842eb7a2d677e37452e7989f1f
Erik Wilson <erik@arcjet.com>
2025-10-03 07:01:16 -0700

    cleanup from comments

--- src/snippets/get-started/nuxt/nuxt-config.ts
+++ src/snippets/get-started/nuxt/nuxt-config.ts
@@ -2,5 +2,16 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  // My Nuxt config
-})
+  arcjet: {
+    // @ts-expect-error
+    key: process.env.ARCJET_KEY
+  },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@arcjet/nuxt",
+  ],
+  typescript: {
+    strict: true,
+  },
+});
