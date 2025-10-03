31fa86f482f09e67293bd70c892cd0918ca26b52
Erik Wilson <erik@arcjet.com>
2025-10-03 07:01:16 -0700

    cleanup from comments

--- src/snippets/get-started/nuxt/Step3Config.js
+++ src/snippets/get-started/nuxt/Step3Config.js
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineNuxtConfig } from "nuxt/config";
-// import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/nuxt";
 
 export default defineNuxtConfig({
+  // @ts-expect-error
   arcjet: {
     key: process.env.ARCJET_KEY,
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@arcjet/nuxt"],
+  modules: [
+    "@arcjet/nuxt"
+  ],
 });
