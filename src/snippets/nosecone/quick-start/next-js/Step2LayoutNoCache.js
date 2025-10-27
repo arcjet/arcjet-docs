971b7cfd616ecc0ce60f1a72dde19ea10b6c7611
David Mytton <david@arcjet.com>
2025-10-27 17:31:37 -0400

    chore: drop Next.js 14 (#701)

--- src/snippets/nosecone/quick-start/next-js/Step2LayoutNoCache.js
+++
@@ -1,8 +0,0 @@
-import { unstable_noStore as noStore } from "next/cache";
-
-export default function RootLayout(props) {
-  // Opt-out of static generation for every page so the CSP nonce can be applied
-  noStore();
-
-  // ...
-}
--- src/snippets/nosecone/quick-start/next-js/Step2LayoutNoCache.ts
+++
@@ -1,9 +0,0 @@
-import { unstable_noStore as noStore } from "next/cache";
-import type { PropsWithChildren } from "react";
-
-export default function RootLayout(props: PropsWithChildren) {
-  // Opt-out of static generation for every page so the CSP nonce can be applied
-  noStore();
-
-  // ...
-}
