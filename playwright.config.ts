473c77d3df56d27c5ba454d4b6882302727f0f2b
Quinn Blenkinsop <quinn@arcjet.com>
2025-12-09 21:53:39 +0000

    chore(playwright): try disabling aliasing for some larger images

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,14 @@ let webServer: PlaywrightTestConfig["webServer"] = undefined;
 if (!baseURL) {
   baseURL = "http://localhost:4321";
   webServer = {
-    command: "npm run dev",
+    command:
+      "npm run astro build && npm run astro -- preview --port 4321 --host 0.0.0.0",
     env: {
       // We need an Arcjet key for the dev server to run, but it doesn't
       // actually need to be valid for the tests to run.
       ARCJET_KEY: "ajkey_dummy",
+      // See astro.config.mts for context.
+      ASTRO_FORCE_NODE_ADAPTER: "1",
     },
     port: 4321,
     reuseExistingServer: !process.env.CI,
@@ -30,15 +33,24 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: [
+            "--font-render-hinting=none",
+            "--disable-lcd-text",
+            "--disable-font-subpixel-positioning",
+          ],
+        },
+      },
     },
   ],
   reporter: [["html", { host: "0.0.0.0" }]],
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   testDir: "./tests",
   use: {
     baseURL,
-    headless: !!process.env.CI,
+    headless: true,
     trace: "on-first-retry",
   },
   workers: process.env.CI ? 2 : undefined,
