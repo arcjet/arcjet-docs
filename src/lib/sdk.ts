3d322765177f4c63ac1435781f8f132b537da950
Quinn Blenkinsop <quinn@arcjet.com>
2026-03-03 09:10:46 -0800

    chore: replace React SDK icons with Astro SdkIcon component (#772)

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -1,16 +1,4 @@
-import type { ElementType } from "react";
 import type { FrameworkKey } from "@/lib/prefs";
-import { Astro as IconAstro } from "@/components/icons/tech/Astro";
-import { Bun as IconBun } from "@/components/icons/tech/Bun";
-import { Deno as IconDeno } from "@/components/icons/tech/Deno";
-import { Fastify as IconFastify } from "@/components/icons/tech/Fastify";
-import { NestJs as IconNestJs } from "@/components/icons/tech/NestJs";
-import { NextJs as IconNextJs } from "@/components/icons/tech/NextJs";
-import { NodeJs as IconNodeJs } from "@/components/icons/tech/NodeJs";
-import { Nuxt as IconNuxt } from "@/components/icons/tech/Nuxt";
-import { ReactRouter as IconReactRouter } from "@/components/icons/tech/ReactRouter";
-import { Remix as IconRemix } from "@/components/icons/tech/Remix";
-import { SvelteKit as IconSvelteKit } from "@/components/icons/tech/SvelteKit";
 
 /**
  * Keys for all of the valid Arcjet SDKs.
@@ -49,10 +37,6 @@ type ArcjetSdk<TKey extends ArcjetSdkKey = ArcjetSdkKey> = {
    * Human readable name of the SDK
    */
   readonly label: string;
-  /**
-   * React icon component for the SDK
-   */
-  readonly ReactIcon: ElementType | null;
 };
 
 /**
@@ -63,73 +47,61 @@ const ARCJET_SDKS = {
     key: "astro",
     label: "Astro",
     legacyFrameworkKey: "astro",
-    ReactIcon: IconAstro,
   },
   bun: {
     key: "bun",
     label: "Bun",
     legacyFrameworkKey: "bun",
-    ReactIcon: IconBun,
   },
   deno: {
     key: "deno",
     label: "Deno",
     legacyFrameworkKey: "deno",
-    ReactIcon: IconDeno,
   },
   fastify: {
     key: "fastify",
     label: "Fastify",
     legacyFrameworkKey: "fastify",
-    ReactIcon: IconFastify,
   },
   nest: {
     key: "nest",
     label: "NestJS",
     legacyFrameworkKey: "nest-js",
-    ReactIcon: IconNestJs,
   },
   next: {
     key: "next",
     label: "Next.js",
     legacyFrameworkKey: "next-js",
-    ReactIcon: IconNextJs,
   },
   node: {
     key: "node",
     label: "Node.js",
     legacyFrameworkKey: "node-js",
-    ReactIcon: IconNodeJs,
   },
   nuxt: {
     key: "nuxt",
     label: "Nuxt",
     legacyFrameworkKey: "nuxt",
-    ReactIcon: IconNuxt,
   },
   python: {
     key: "python",
     label: "Python",
     legacyFrameworkKey: null,
-    ReactIcon: null,
   },
   "react-router": {
     key: "react-router",
     label: "React Router",
     legacyFrameworkKey: "react-router",
-    ReactIcon: IconReactRouter,
   },
   remix: {
     key: "remix",
     label: "Remix",
     legacyFrameworkKey: "remix",
-    ReactIcon: IconRemix,
   },
   sveltekit: {
     key: "sveltekit",
     label: "SvelteKit",
     legacyFrameworkKey: "sveltekit",
-    ReactIcon: IconSvelteKit,
   },
 } as const satisfies { [TKey in ArcjetSdkKey]: ArcjetSdk<TKey> };
 
